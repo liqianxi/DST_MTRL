5407bbc7cfd3293210c9ca343812e4e2f88ef9c5
liqianxi <qianxi@ualberta.ca>
2023-07-23 14:08:19 -0700

    debug first stage finish, some parts need to be moved to GPU
    Former-commit-id: 01abb295afd499dde7fc70b57658fc81348fd5b0

--- starter/mt_must_sac.py
+++ starter/mt_must_sac.py
@@ -8,7 +8,7 @@ import time,copy
 import os.path as osp
 
 import numpy as np
-
+import torch.multiprocessing as mp
 from torchrl.utils import get_args
 from torchrl.utils import get_params
 from torchrl.env import get_env
@@ -51,23 +51,10 @@ os.environ['VECLIB_MAXIMUM_THREADS'] = str(CPU_NUM)
 os.environ['NUMEXPR_NUM_THREADS'] = str(CPU_NUM)
 torch.set_num_threads(CPU_NUM)
 
-# def convert_neuron_masks_to_weight_mask(weight, neuron_masks, mode):
-#     assert mode in ["row","col"], "Must specify an operation"
-#     full_mask = weight
-
-#     if mode == "row":
-#         full_mask = torch.mul(full_mask.T, neuron_masks).T
-
-#     else: 
-#         full_mask = torch.mul(full_mask, neuron_masks)
-
-#     return full_mask
-
 def random_initialize_masks(network, pruning_ratio):
     neuron_mask_list = []
     all_layer_weight_shape = []
-    # print(network.base.fcs) [Linear(in_features=33, out_features=400, bias=True), 
-    # Linear(in_features=400, out_features=400, bias=True)]
+
     for each_layer in network.base.fcs:
         neurons = each_layer.bias.shape[0]
         all_layer_weight_shape.append(each_layer.weight.shape)
@@ -79,33 +66,7 @@ def random_initialize_masks(network, pruning_ratio):
 
     all_layer_weight_shape.append(network.last.weight.shape)
 
-    #weight_
-    # weight_array = [torch.ones(i) for i in all_layer_weight_shape]
-
-
-    # for idx in range(len(neuron_mask_list)):
-    #     pre_layer_weight = convert_neuron_masks_to_weight_mask(weight_array[idx], 
-    #                                                            neuron_mask_list[idx], 
-    #                                                            mode="row")
-    #     weight_array[idx] = pre_layer_weight
-
-    #     post_layer_weight = convert_neuron_masks_to_weight_mask(weight_array[idx+1], 
-    #                                                             neuron_mask_list[idx], 
-    #                                                             mode="col")
-    #     weight_array[idx+1] = post_layer_weight
-
-    #bias_mask_array = neuron_mask_list
-
-    # So, for this task, mask out some neurons from the weight and bias tensors.
-    # The mask of tensor is equivalent to the mask of neurons.
-    # The mask of weight is applying neuron mask along rows or columns of the weight matrix, depends
-    # on which matrix this neuron is affecting.
     return neuron_mask_list
-
-
-
-        
-
 
 def experiment(args):
 
@@ -141,10 +102,8 @@ def experiment(args):
     params['net']['base_type']=networks.MLPBase
 
 
-    import torch.multiprocessing as mp
+    
     mp.set_start_method('spawn', force=True)
-
-    from torchrl.networks.init import normal_init
 
     example_ob = env.reset()
     example_embedding = env.active_task_one_hot
@@ -154,8 +113,6 @@ def experiment(args):
     # The policy network 
     # Input: S,onehot(task)
 
-    # print(env.observation_space.shape)
-    # print(env.action_space.shape)
     # (19,)
     # (4,)
 
@@ -197,11 +154,8 @@ def experiment(args):
     # target nets should follow the gradients of Q nets, but with discount factor.
     # We use the same mask with their corresponding Q networks.
 
-    #print(params['net']["hidden_shapes"])
     policy_mask_generator = networks.MaskGeneratorNet(
         base_type=networks.MLPBase,
-        traj_encoder_hidden_shape = params['traj_encoder']['hidden_shapes'],
-        input_shape=env.observation_space.shape[0] * params['general_setting']["epoch_frames"],
         em_hidden_shapes=params['task_embedding']['em_hidden_shapes'],
         em_input_shape=np.prod(example_embedding.shape),
         num_layers=2, ##:
@@ -211,44 +165,8 @@ def experiment(args):
         device="cpu"
         )
 
-    # obs = torch.from_numpy(env.observation_space.sample()).float()
-    # obs = torch.cat([obs]*params['general_setting']["epoch_frames"]).float()
-    # emb = torch.from_numpy(example_embedding).float()
-
-    #print([module for module in policy_mask_generator.modules()])
-    #print(obs.shape, emb.shape) # (19,) (10,)
-    #mask_list = policy_mask_generator(obs,emb)
-    #print("mask_list",mask_list)
-    """
-    [MaskGeneratorNet(
-    (base): MLPBase(
-        (fc0): Linear(in_features=3800, out_features=400, bias=True)
-        (fc1): Linear(in_features=400, out_features=400, bias=True)
-    )
-    (em_base): MLPBase(
-        (fc0): Linear(in_features=10, out_features=400, bias=True)
-        (fc1): Linear(in_features=400, out_features=400, bias=True)
-    )
-    (gating_weight_fc_0): Linear(in_features=400, out_features=400, bias=True)
-    (gating_weight_cond_last): Linear(in_features=400, out_features=400, bias=True)
-    (gating_weight_last): Linear(in_features=400, out_features=400, bias=True)
-    ), MLPBase(
-    (fc0): Linear(in_features=3800, out_features=400, bias=True)
-    (fc1): Linear(in_features=400, out_features=400, bias=True)
-    ), Linear(in_features=3800, out_features=400, bias=True), Linear(in_features=400, out_features=400, bias=True), MLPBase(
-    (fc0): Linear(in_features=10, out_features=400, bias=True)
-    (fc1): Linear(in_features=400, out_features=400, bias=True)
-    ), Linear(in_features=10, out_features=400, bias=True), Linear(in_features=400, out_features=400, bias=True), Linear(in_features=400, out_features=400, bias=True), Linear(in_features=400, out_features=400, bias=True), Linear(in_features=400, out_features=400, bias=True)]
-    
-    
-    
-    """
-
-
     qf1_mask_generator = networks.MaskGeneratorNet(
         base_type=networks.MLPBase,
-        traj_encoder_hidden_shape = params['traj_encoder']['hidden_shapes'],
-        input_shape=env.observation_space.shape[0] * params['general_setting']["epoch_frames"],
         em_hidden_shapes=params['task_embedding']['em_hidden_shapes'],
         em_input_shape=np.prod(example_embedding.shape),
         num_layers=2, ##:
@@ -258,8 +176,6 @@ def experiment(args):
         device="cpu")
     qf2_mask_generator = networks.MaskGeneratorNet(
         base_type=networks.MLPBase,
-        traj_encoder_hidden_shape = params['traj_encoder']['hidden_shapes'],
-        input_shape=env.observation_space.shape[0] * params['general_setting']["epoch_frames"],
         em_hidden_shapes=params['task_embedding']['em_hidden_shapes'],
         em_input_shape=np.prod(example_embedding.shape),
         num_layers=2, ##:
@@ -292,27 +208,23 @@ def experiment(args):
     manager = mp.Manager()
     # This is used to train the encoder and compare traj similarity.
     state_trajectory = manager.dict()
+
     for task_idx in range( env.num_tasks):
         state_trajectory[task_idx] = []
-
 
     # Mask buffer, stores the current masks for each layer, 
     # for each task and for each network type(Q1, Q2, policy).
     # Initialize the binary mask for all the weights and bias, make sure
     # follow the pruning ratio requirement.
 
-    
-
     all_mask_buffer = {}
 
-    # mask_buffer = {"Q1":{}, "Q2":{}, "Policy":{}}
     for net_type in ["Q1","Q2","Policy"]:
         mask_buffer = manager.dict()
 
         for task_idx in range( env.num_tasks):
             mask_buffer[task_idx] = []
 
-        
             if net_type == "Q1":
                 net = qf1
             elif net_type == "Q2":
@@ -321,21 +233,9 @@ def experiment(args):
                 net = pf
 
             neuron_masks = random_initialize_masks(net, pruning_ratio)
-
             mask_buffer[task_idx] = neuron_masks
-            #print("mask_buffer[net_type][each_task]",mask_buffer[net_type][each_task])
 
         all_mask_buffer[net_type] = mask_buffer       
-        #print(id(all_mask_buffer[net_type]))
-    #mask_buffer = mask_buffer.to(device)
-    # Now we have a mask buffer like this:
-    # {"Q1":{"0":[
-    #                  [[layer1_Weights],[layer2_weights]..], 
-    #                ],
-    #
-    # 
-    # ,..."49":[]},"Q2":{...},"Policy":{...}}
-
 
     if RESTORE:
         with open(osp.join(osp.join(logger.work_dir,"model"), "replay_buffer.pkl"), 'rb') as f:
@@ -343,13 +243,8 @@ def experiment(args):
 
     params['general_setting']['replay_buffer'] = replay_buffer
     params['general_setting']['state_trajectory'] = state_trajectory
-    #params['general_setting']['mask_buffer'] = mask_buffer
-    #print("state_trajectory id",id(state_trajectory))
     epochs = params['general_setting']['pretrain_epochs'] + \
         params['general_setting']['num_epochs']
-
-    
-
 
     params['general_setting']['collector'] = AsyncMultiTaskParallelCollectorUniform(
         env=env, pf=pf, replay_buffer=replay_buffer,state_trajectory=state_trajectory,
@@ -371,8 +266,6 @@ def experiment(args):
                            "qf1_mask_generator": qf1_mask_generator,
                            "qf2_mask_generator": qf2_mask_generator},
     """
-
-
     
     agent = MUST_SAC(
         pf = pf,
--- torchrl/networks/nets.py
+++ torchrl/networks/nets.py
@@ -101,23 +101,13 @@ class MaskedNet(nn.Module):
         net_last_init_func(self.last)
 
     def forward(self, x, neuron_masks):
-
-        out = None
+        # Has examined, this forward should be correct - 0723 qianxi.
         mask_out = x
-
         for idx, layer in enumerate(self.base.fcs):
-
-            out = layer(mask_out)
-
-
-            tmp = neuron_masks[idx]
-
-            mask_out = out * tmp 
-            mask_out = self.activation_func(mask_out)
+            mask_out = self.activation_func(layer(mask_out) * neuron_masks[idx])
 
         out = self.last(mask_out)
         return out
-
 
 
 class FlattenNet(MaskedNet):
@@ -128,226 +118,6 @@ class FlattenNet(MaskedNet):
 
 def null_activation(x):
     return x
-
-class ModularGatedCascadeCondNet(nn.Module):
-    def __init__(self, output_shape,
-            base_type, em_input_shape, input_shape,
-            em_hidden_shapes,
-            hidden_shapes,
-
-            num_layers, num_modules,
-
-            module_hidden,
-
-            gating_hidden, num_gating_layers,
-
-            # gated_hidden
-            add_bn = True,
-            pre_softmax = False,
-            cond_ob = True,
-            module_hidden_init_func = init.basic_init,
-            last_init_func = init.uniform_init,
-            activation_func = F.relu,
-             **kwargs ):
-
-        super().__init__()
-
-        self.base = base_type( 
-                        last_activation_func = null_activation,
-                        input_shape = input_shape,
-                        activation_func = activation_func,
-                        hidden_shapes = hidden_shapes,
-                        **kwargs )
-        self.em_base = base_type(
-                        last_activation_func = null_activation,
-                        input_shape = em_input_shape,
-                        activation_func = activation_func,
-                        hidden_shapes = em_hidden_shapes,
-                        **kwargs )
-
-        self.activation_func = activation_func
-
-        module_input_shape = self.base.output_shape
-        self.layer_modules = []
-
-        self.num_layers = num_layers
-        self.num_modules = num_modules
-        #print("self.num_layers",self.num_layers)
-        assert 1==2
-        for i in range(num_layers):
-            layer_module = []
-            for j in range( num_modules ):
-                fc = nn.Linear(module_input_shape, module_hidden)
-                module_hidden_init_func(fc)
-                if add_bn:
-                    module = nn.Sequential(
-                        nn.BatchNorm1d(module_input_shape),
-                        fc,
-                        nn.BatchNorm1d(module_hidden)
-                    )
-                else:
-                    module = fc
-
-                layer_module.append(module)
-                self.__setattr__("module_{}_{}".format(i,j), module)
-
-            module_input_shape = module_hidden
-            self.layer_modules.append(layer_module)
-
-        self.last = nn.Linear(module_input_shape, output_shape)
-        last_init_func( self.last )
-
-        assert self.em_base.output_shape == self.base.output_shape, \
-            "embedding should has the same dimension with base output for gated" 
-        gating_input_shape = self.em_base.output_shape
-        self.gating_fcs = []
-        for i in range(num_gating_layers):
-            gating_fc = nn.Linear(gating_input_shape, gating_hidden)
-            module_hidden_init_func(gating_fc)
-            self.gating_fcs.append(gating_fc)
-            self.__setattr__("gating_fc_{}".format(i), gating_fc)
-            gating_input_shape = gating_hidden
-
-        self.gating_weight_fcs = []
-        self.gating_weight_cond_fcs = []
-
-        self.gating_weight_fc_0 = nn.Linear(gating_input_shape,
-                    num_modules * num_modules )
-        last_init_func( self.gating_weight_fc_0)
-        # self.gating_weight_fcs.append(self.gating_weight_fc_0)
-        
-        for layer_idx in range(num_layers-2):
-            gating_weight_cond_fc = nn.Linear((layer_idx+1) * \
-                                               num_modules * num_modules,
-                                              gating_input_shape)
-            module_hidden_init_func(gating_weight_cond_fc)
-            self.__setattr__("gating_weight_cond_fc_{}".format(layer_idx+1),
-                             gating_weight_cond_fc)
-            self.gating_weight_cond_fcs.append(gating_weight_cond_fc)
-
-            gating_weight_fc = nn.Linear(gating_input_shape,
-                                         num_modules * num_modules)
-            last_init_func(gating_weight_fc)
-            self.__setattr__("gating_weight_fc_{}".format(layer_idx+1),
-                             gating_weight_fc)
-            self.gating_weight_fcs.append(gating_weight_fc)
-
-        
-        self.gating_weight_cond_last = nn.Linear((num_layers-1) * \
-                                                 num_modules * num_modules,
-                                                 gating_input_shape)
-        module_hidden_init_func(self.gating_weight_cond_last)
-
-        self.gating_weight_last = nn.Linear(gating_input_shape, num_modules)
-        last_init_func( self.gating_weight_last )
-
-        self.pre_softmax = pre_softmax
-        self.cond_ob = cond_ob
-
-    def forward(self, x, embedding_input, return_weights = False):
-        # Return weights for visualization
-        out = self.base(x)
-        embedding = self.em_base(embedding_input)
-
-        if self.cond_ob:
-            embedding = embedding * out
-
-        out = self.activation_func(out)
-        #self.gating_fcs [Linear(in_features=400, out_features=256, bias=True), Linear(in_features=256, out_features=256, bias=True)]
-        #print("self.gating_fcs",self.gating_fcs) 
-        #print("self.gating_fcs",self.gating_fcs)
-        if len(self.gating_fcs) > 0:
-            embedding = self.activation_func(embedding)
-            for fc in self.gating_fcs[:-1]:
-                embedding = fc(embedding)
-                embedding = self.activation_func(embedding)
-            embedding = self.gating_fcs[-1](embedding)
-
-        base_shape = embedding.shape[:-1]
-        #print("base_shape",base_shape)
-        weights = []
-        flatten_weights = []
-
-        raw_weight = self.gating_weight_fc_0(self.activation_func(embedding))  # torch.Size([128, 10, 4])
-
-        weight_shape = base_shape + torch.Size([self.num_modules,
-                                                self.num_modules])
-        flatten_shape = base_shape + torch.Size([self.num_modules * \
-                                                self.num_modules])
-
-        raw_weight = raw_weight.view(weight_shape)
-
-        softmax_weight = F.softmax(raw_weight, dim=-1)  # torch.Size([128, 10, 2, 2])
-        weights.append(softmax_weight)
-        if self.pre_softmax:
-            flatten_weights.append(raw_weight.view(flatten_shape))
-        else:
-            flatten_weights.append(softmax_weight.view(flatten_shape))
-        # if case 4 layers:
-        #[Linear(in_features=256, out_features=16, bias=True), Linear(in_features=256, out_features=16, bias=True)] 
-        #[Linear(in_features=16, out_features=256, bias=True), Linear(in_features=32, out_features=256, bias=True)]
-        # print(self.gating_weight_fcs, self.gating_weight_cond_fcs)
-        for gating_weight_fc, gating_weight_cond_fc in zip(self.gating_weight_fcs, self.gating_weight_cond_fcs):
-            cond = torch.cat(flatten_weights, dim=-1)
-            if self.pre_softmax:
-                cond = self.activation_func(cond)
-            cond = gating_weight_cond_fc(cond)# W_up (4*4 x D)
-            cond = cond * embedding
-            cond = self.activation_func(cond)
-
-            raw_weight = gating_weight_fc(cond) # W_down
-            raw_weight = raw_weight.view(weight_shape)
-            softmax_weight = F.softmax(raw_weight, dim=-1)
-            weights.append(softmax_weight)
-            if self.pre_softmax:
-                flatten_weights.append(raw_weight.view(flatten_shape))
-            else:
-                flatten_weights.append(softmax_weight.view(flatten_shape))
-        
-        cond = torch.cat(flatten_weights, dim=-1)
-        if self.pre_softmax:
-            cond = self.activation_func(cond)
-        cond = self.gating_weight_cond_last(cond)
-        cond = cond * embedding
-        cond = self.activation_func(cond)
-
-        raw_last_weight = self.gating_weight_last(cond)
-        last_weight = F.softmax(raw_last_weight, dim = -1)
-
-        module_outputs = [(layer_module(out)).unsqueeze(-2) \
-                for layer_module in self.layer_modules[0]]
-
-        module_outputs = torch.cat(module_outputs, dim = -2 )  # torch.Size([128, 10, 2, 256])
-
-        # [TODO] Optimize using 1 * 1 convolution.
-
-        for i in range(self.num_layers - 1):
-            new_module_outputs = []
-            for j, layer_module in enumerate(self.layer_modules[i + 1]):
-                module_input = (module_outputs * \
-                    weights[i][..., j, :].unsqueeze(-1)).sum(dim=-2)
-
-                module_input = self.activation_func(module_input)
-                new_module_outputs.append((
-                        layer_module(module_input)
-                ).unsqueeze(-2))
-
-            module_outputs = torch.cat(new_module_outputs, dim = -2)
-
-        out = (module_outputs * last_weight.unsqueeze(-1)).sum(-2)
-        out = self.activation_func(out)
-        out = self.last(out)
-
-        if return_weights:
-            return out, weights, last_weight
-        return out
-
-
-class FlattenModularGatedCascadeCondNet(ModularGatedCascadeCondNet):
-    def forward(self, input, embedding_input, return_weights = False):
-        out = torch.cat( input, dim = -1 )
-        return super().forward(out, embedding_input, return_weights = return_weights)
-
  
 class BootstrappedNet(Net):
     def __init__(self, output_shape, 
@@ -381,10 +151,9 @@ class FlattenBootstrappedNet(BootstrappedNet):
 
 class MaskGeneratorNet(nn.Module):
     def __init__(self,
-            base_type, em_input_shape, input_shape,
+            base_type, em_input_shape,
             em_hidden_shapes,
             hidden_shapes,
-            traj_encoder_hidden_shape,
             num_layers,
             trajectory_encoder,
             pruning_ratio,
@@ -397,13 +166,7 @@ class MaskGeneratorNet(nn.Module):
 
         #Note: Embedding base is the network part that converts a full trajectory into
         # a D-dim vector.
-        #TODO: Shall be replaced by an encoder.
-        # self.base = base_type( 
-        #                 last_activation_func = null_activation,
-        #                 input_shape = input_shape,
-        #                 activation_func = activation_func,
-        #                 hidden_shapes = traj_encoder_hidden_shape
-        #                 ).float()
+
 
         self.base = trajectory_encoder
         self.pruning_ratio = pruning_ratio
@@ -473,12 +236,10 @@ class MaskGeneratorNet(nn.Module):
     def get_learnable_params(self):
         dict = self.__dict__["_modules"]
         param_list = []
-        #print(dict)
+
         for key, value in dict.items():
-            #print(key)
             if key != "base":
                 param = [i for i in value.parameters()]
-                #print(param)
                 param_list += param
             
         return param_list
@@ -488,22 +249,17 @@ class MaskGeneratorNet(nn.Module):
     def forward(self, x, embedding_input):
         # Here x is a trajectory of shape [traj_length, dim_of_each_state]
         # Return weights for visualization
-        #print("x",x)
-        #print(x.shape) # torch.Size([201, 19])
-        # Trajectory encoder embedding
-        #print("self.base",self.base)
 
+        # Trajectory encoder embedding
         out = self.base.encode_lstm(x)
         #print("genout.shape",out.shape)genout.shape torch.Size([128])
 
-        #
         # Task one hot embedding
         embedding = self.em_base(embedding_input)
 
         # Element wise multi
         embedding = embedding * out
 
-        #weight_shape = torch.Size([self.layer_neurons])
         task_probs_masks = []
 
         activated = self.activation_func(embedding).to(self.device)
@@ -523,7 +279,6 @@ class MaskGeneratorNet(nn.Module):
         raw_weight = F.softmax(raw_weight, dim=-1)
         task_probs_masks.append(raw_weight)
 
-
         idx = 1
         for gating_weight_fc, gating_weight_cond_fc in zip(self.gating_weight_fcs, self.gating_weight_cond_fcs):
 
@@ -541,16 +296,6 @@ class MaskGeneratorNet(nn.Module):
             raw_weight = F.softmax(raw_weight, dim=-1)
             task_probs_masks.append(raw_weight)
 
-            # pruned_mask = self.keep_topk(raw_weight, self.pruning_ratio, layer_neurons)
-            # # Here, shape the task_probs_masks to [0,1]
-            # non_zero_indices = pruned_mask.nonzero(as_tuple=False).squeeze()
-
-            # softmax_weight[non_zero_indices] = F.softmax(pruned_mask[non_zero_indices], dim=-1)
-            
-            #task_probs_masks.append(softmax_weight)
-        
-        #print(softmax_weight)
-
         cond = self.gating_weight_cond_last(raw_weight)  # W_up (neurons x D) * p^l
         cond = cond * embedding # (W_up * p^l) * embedding
         cond = self.activation_func(cond)  #RELU (cond)
@@ -559,18 +304,8 @@ class MaskGeneratorNet(nn.Module):
         raw_last_weight = self.gating_weight_last(cond) 
 
         # Change the prob to [0,1].
-        # TODO: need to verify the correstness of this line.
-        # print(raw_last_weight.shape)
-        # print(self.pruning_ratio)
-        # print(self.layer_neurons[idx])
-        # pruned_mask = self.keep_topk(raw_last_weight.flatten(), 
-        #                              self.pruning_ratio, 
-        #                              self.layer_neurons[idx])
-        # #print(pruned_mask)
         raw_last_weight = F.softmax(raw_last_weight, dim=-1)
-        # sum_up = pruned_mask.sum()
-        # print("sum_up",sum_up)
-        # pruned_mask /= sum_up
+
         task_probs_masks.append(raw_last_weight)   
 
         task_binary_masks = []
