d83f9781b9d04fc2c66c8649aae5d2df017a9296
liqianxi <qianxi@ualberta.ca>
2023-11-11 15:46:04 -0500

    new

--- sync_wandb.sh
+++ sync_wandb.sh
@@ -4,6 +4,6 @@
 for folder in /home/qianxi/scratch/sparse_training/sep_t3s/DST_RL/wandb/offline-*; do
     if [ -d "$folder" ]; then
         echo "Syncing $folder"
-        wandb sync --include-offline "$folder"
+        wandb sync --sync-all --include-offline "$folder"
     fi
 done
