436f611ad2344d6e969fdcf34306daf3ec56d6cc
liqianxi <qianxi@ualberta.ca>
2024-01-23 01:06:09 -0500

    fix mask bug

--- t3s_scripts/must/mt10/selected_fix_sweep.sh
+++ t3s_scripts/must/mt10/selected_fix_sweep.sh
@@ -4,50 +4,16 @@
 for seed in {3..4}; 
 do
 
-    for pruning_ratio in {0.1,0.7};
+    for pruning_ratio in {0.3,0.7};
     do
 
-        # First, more than 1 task.
-        for selected_task in {2,4,10};
-        do 
-            for itv in {50,100,200,400}
-            do
-                sbatch selected_fix_sweep_single.sh $seed $itv $pruning_ratio 1e-3 0 0 $selected_task 1
-            done
-        done
-
         # Second, 1 task.
-        for task_id in {0..9}
+        for task_id in {0..2}
         do
-            for itv in {50,100,200,400}
+            for itv in {50,100,400}
             do
                 sbatch selected_fix_sweep_single.sh $seed $itv $pruning_ratio 1e-3 0 0 1 $task_id
             done
-        done
-    done
-
-
-done
-
-# Second, use SL loss and RL loss
-for seed in {3..4}; 
-do
-
-    for pruning_ratio in {0.1,0.7};
-    do
-        for gen_lr in {1e-3,1e-2};
-        do
-            for use_trajectory_info in {0,1};
-            do
-                for selected_task in {2,4,10};
-                do 
-                    for itv in {50,100,200,400}
-                    do
-                        sbatch selected_fix_sweep_single.sh $seed $itv $pruning_ratio $gen_lr 1 $use_trajectory_info $selected_task 1
-                    done
-                done
-            done
-
         done
     done
 
--- t3s_scripts/must/mt10/selected_fix_sweep_single.sh
+++ t3s_scripts/must/mt10/selected_fix_sweep_single.sh
@@ -9,14 +9,14 @@
 #SBATCH --account=rrg-mtaylor3
 #SBATCH --output=/home/qianxi/scratch/slurm_log/must/must_sweep/%A.out
 #SBATCH --mail-user=qianxi@ualberta.ca
-#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=ALL
 
 module load python/3.8
 module load cuda
 
 source /home/qianxi/scratch/sparse_training/dec_must/must_env/bin/activate;
 cd /home/qianxi/scratch/sparse_training/dec_must/DST_RL
-wandb enabled
+wandb offline
 export WANDB_API_KEY=b363daac0bf911130cb2eff814388eaf99942a0b
 SECONDS=0
 
@@ -26,8 +26,8 @@ echo "Use seed $1"
 export GROUP=MT10_MUSTSAC
 export NAME="10runs_MUST_fixgoal_MT10_exp2_seed${1}"
 export TASK_SAMPLE_NUM=$7 
-ID="1213_itv${2}_pr${3}_sllr${4}_slloss${5}_trajinfo${6}_selected${7}_select-tsk-id${8}"
-GROUP_NAME="1213_sweep"
+ID="0120_itv${2}_pr${3}_sllr${4}_slloss${5}_trajinfo${6}_selected${7}_select-tsk-id${8}"
+GROUP_NAME="0120_sweep"
 python starter/mt_must_sac.py --config meta_config/must_configs/mt10/must_mtsac.json --worker_nums $7 --eval_worker_nums $7 --seed $1 --id $ID --mask_update_interval $2 --pruning_ratio $3 --generator_lr $4 --use_sl_loss $5 --use_trajectory_info $6  --wandb_group_name $GROUP_NAME --selected_task_amount $7  --specify_single_task $8
 duration=$SECONDS
 echo "$duration seconds elapsed."
